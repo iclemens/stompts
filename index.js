525d71a2ddeb6f9b8850b7ed8af19baffbb8bcc7
Ivar Clemens <post@ivarclemens.nl>
2017-09-18 15:30:01 +0200

    removed node stuff from index.js

--- index.js
+++ index.js
@@ -8,8 +8,5 @@
 // Root of the `stompjs module`
 
 var Stomp = require('./lib/stomp.js');
-var StompNode = require('./lib/stomp-node.js');
 
 module.exports = Stomp.Stomp;
-module.exports.overTCP = StompNode.overTCP;
-module.exports.overWS = StompNode.overWS;
